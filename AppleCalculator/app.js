0b4bc0c8242547d062d15b3ed906f228ed51b36a
shivangii123 <130161239+shivangii123@users.noreply.github.com>
2026-05-11 18:48:26 +0530

    Enhance calculator logic for evaluated state
    Added functionality to reset calculation after '=' is pressed and handle fresh calculations.

--- AppleCalculator/app.js
+++ AppleCalculator/app.js
@@ -1,36 +1,48 @@
-
-
-
-
 let inpElm = document.querySelector('input');
 let btns = document.querySelectorAll('button');
 
+let evaluated = false; // tracks if "=" was pressed
+
 
 for(let button of btns){
+
     button.addEventListener('click', function(e){
-        // console.log(e.target.innerText);
+
         let btnVl = e.target.textContent;
 
         if(btnVl === 'AC'){
-            inpElm.value ="" ;
+            inpElm.value = "";
+            evaluated = false;
         }
 
-        //inpElm.value -> string , string are immuatble
         else if(btnVl === 'D'){
-            inpElm.value = inpElm.value.slice(0,-1);// so we reassign value to string..
+            inpElm.value = inpElm.value.slice(0,-1);
         }
+
         else if(btnVl === '='){
-            try{ // sahi kaam mein--> evalaute expression
+            try{
                 inpElm.value = eval(inpElm.value);
+                evaluated = true; // result displayed
             }
             catch(e){
-                inpElm.value = "Invalid" ;
+                inpElm.value = "Invalid";
+            }
+        }
+
+        else{
+
+            // if result already shown and user presses number
+            // start fresh calculation
+            if(evaluated){ //if "=" pressed
+                if(!isNaN(btnVl)){ //if number pressed
+                    inpElm.value = ""; //starts calculation fresh
+                }
+                evaluated = false;
             }
 
-        }
-        else{
-            inpElm.value += btnVl ;
+            inpElm.value += btnVl;
         }
 
     })
+
 }
